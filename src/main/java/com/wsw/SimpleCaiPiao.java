2edddf3dbe948ed4173d337a95b754db70001c74
王思旺 <wangsw@sit.com.cn>
2018-09-17 12:18:05 +0800

    完成爬取数据(修改BUG)

--- src/main/java/com/wsw/SimpleCaiPiao.java
+++ src/main/java/com/wsw/SimpleCaiPiao.java
@@ -27,7 +27,7 @@ public class SimpleCaiPiao {
         //创建一个GET请求
         HttpGet httpGet1 = new HttpGet("http://kaijiang.500.com/shtml/ssq/03002.shtml");
         httpGet1.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36");
-        httpGet1.addHeader("Cookie", "_gat=1; ck_regchanel=baidu; regfrom=0%7Cala%7Cbaidu; sdc_session=1537106735463; bdshare_firstime=1537106735537; _jzqy=1.1537106736.1537106736.1.jzqsr=baidu.-; _jzqckmp=1; __utmt=1; seo_key=baidu%7C%7Chttps://www.baidu.com/link?url=D1N4tf-fgloiO1ZXzZyd9E7e4sk2cVPEu2d0Zn4X9STD0VdkWnLhkGgjbGxuHG_E&wd=&eqid=b4e1d05a00005443000000065b9e6325; ck_RegUrl=kaijiang.500.com; ck_RegFromUrl=http%3A//kaijiang.500.com/%3F0_ala_baidu; WT_FPC=ids=undefined:lv=1537106765817:ss=1537106735461; sdc_userflag=1537106735463::1537106765829::5; Hm_lvt_4f816d475bb0b9ed640ae412d6b42cab=1537106735; Hm_lpvt_4f816d475bb0b9ed640ae412d6b42cab=1537106766; _qzja=1.486111922.1537106735602.1537106735602.1537106735603.1537106756731.1537106766010.0.0.0.5.1; _qzjb=1.1537106735602.5.0.0.0; _qzjc=1; _qzjto=5.1.0; _jzqa=1.2403698331785671000.1537106736.1537106736.1537106736.1; _jzqc=1; _jzqb=1.5.10.1537106736.1; __utma=63332592.1847551773.1537106737.1537106737.1537106737.1; __utmb=63332592.5.10.1537106737; __utmc=63332592; __utmz=63332592.1537106737.1.1.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; motion_id=1537106868057_0.3170700300715521; CLICKSTRN_ID=111.19.39.11-1537106733.570126::E7AFD1B52D3BF81A20263B59DC7C5BC6");
+        httpGet1.addHeader("Cookie", "sdc_session=1537149671465; Hm_lvt_4f816d475bb0b9ed640ae412d6b42cab=1537149672; bdshare_firstime=1537149671855; _qzjc=1; _jzqc=1; _jzqckmp=1; __utmc=63332592; __utmz=63332592.1537149673.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); ck_RegUrl=kaijiang.500.com; ck_RegFromUrl=http%3A//kaijiang.500.com/shtml/ssq/03002.shtml; WT_FPC=id=undefined:lv=1537155732274:ss=1537155732274; _qzja=1.2143396619.1537149671861.1537152882554.1537155732535.1537152882554.1537155732535.0.0.0.4.3; _qzjto=4.3.0; _jzqa=1.2046596389977329400.1537149672.1537152883.1537155733.3; _jzqx=1.1537152883.1537155733.2.jzqsr=kaijiang%2E500%2Ecom|jzqct=/shtml/ssq/03002%2Eshtml.jzqsr=kaijiang%2E500%2Ecom|jzqct=/shtml/ssq/03089%2Eshtml; _qzjb=1.1537155732535.1.0.0.0; __utma=63332592.1675891417.1537149673.1537152883.1537155733.3; sdc_userflag=1537155732277::1537157402642::7; Hm_lpvt_4f816d475bb0b9ed640ae412d6b42cab=1537157403; _jzqb=1.7.10.1537155733.1; __utmt=1; __utmb=63332592.7.10.1537155733; CLICKSTRN_ID=1.80.218.173-1537149664.528767::16B1D937ED722FF6F94810E7758DDA62; motion_id=1537157412260_0.12137685854805791");
         httpGet1.addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
         httpGet1.addHeader("Accept-Encoding", "gzip, deflate");
         try {
@@ -41,16 +41,24 @@ public class SimpleCaiPiao {
             for (int i = 1; i < p.length; i++) {
                 String substring = p[i].substring(0, p[i].indexOf(".shtml"));
                 int i1 = Integer.parseInt(substring);
-                ids.add(i1);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+        execute(httpClient, ids);
+    }
+
+    private static void execute(CloseableHttpClient httpClient, List<Integer> ids) {
         for (Integer id : ids) {
             //创建一个GET请求
-            HttpGet httpGet = new HttpGet("http://kaijiang.500.com/shtml/ssq/"+id+".shtml");
+            HttpGet httpGet =null;
+            if (String.valueOf(id).length()<5) {
+                httpGet = new HttpGet("http://kaijiang.500.com/shtml/ssq/0"+id+".shtml");
+            }else {
+                httpGet = new HttpGet("http://kaijiang.500.com/shtml/ssq/"+id+".shtml");
+            }
             httpGet.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36");
-            httpGet.addHeader("Cookie", "_gat=1; ck_regchanel=baidu; regfrom=0%7Cala%7Cbaidu; sdc_session=1537106735463; bdshare_firstime=1537106735537; _jzqy=1.1537106736.1537106736.1.jzqsr=baidu.-; _jzqckmp=1; __utmt=1; seo_key=baidu%7C%7Chttps://www.baidu.com/link?url=D1N4tf-fgloiO1ZXzZyd9E7e4sk2cVPEu2d0Zn4X9STD0VdkWnLhkGgjbGxuHG_E&wd=&eqid=b4e1d05a00005443000000065b9e6325; ck_RegUrl=kaijiang.500.com; ck_RegFromUrl=http%3A//kaijiang.500.com/%3F0_ala_baidu; WT_FPC=ids=undefined:lv=1537106765817:ss=1537106735461; sdc_userflag=1537106735463::1537106765829::5; Hm_lvt_4f816d475bb0b9ed640ae412d6b42cab=1537106735; Hm_lpvt_4f816d475bb0b9ed640ae412d6b42cab=1537106766; _qzja=1.486111922.1537106735602.1537106735602.1537106735603.1537106756731.1537106766010.0.0.0.5.1; _qzjb=1.1537106735602.5.0.0.0; _qzjc=1; _qzjto=5.1.0; _jzqa=1.2403698331785671000.1537106736.1537106736.1537106736.1; _jzqc=1; _jzqb=1.5.10.1537106736.1; __utma=63332592.1847551773.1537106737.1537106737.1537106737.1; __utmb=63332592.5.10.1537106737; __utmc=63332592; __utmz=63332592.1537106737.1.1.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; motion_id=1537106868057_0.3170700300715521; CLICKSTRN_ID=111.19.39.11-1537106733.570126::E7AFD1B52D3BF81A20263B59DC7C5BC6");
+            httpGet.addHeader("Cookie", "sdc_session=1537149671465; Hm_lvt_4f816d475bb0b9ed640ae412d6b42cab=1537149672; bdshare_firstime=1537149671855; _qzjc=1; _jzqc=1; _jzqckmp=1; __utmc=63332592; __utmz=63332592.1537149673.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); ck_RegUrl=kaijiang.500.com; ck_RegFromUrl=http%3A//kaijiang.500.com/shtml/ssq/03002.shtml; WT_FPC=id=undefined:lv=1537155732274:ss=1537155732274; _qzja=1.2143396619.1537149671861.1537152882554.1537155732535.1537152882554.1537155732535.0.0.0.4.3; _qzjto=4.3.0; _jzqa=1.2046596389977329400.1537149672.1537152883.1537155733.3; _jzqx=1.1537152883.1537155733.2.jzqsr=kaijiang%2E500%2Ecom|jzqct=/shtml/ssq/03002%2Eshtml.jzqsr=kaijiang%2E500%2Ecom|jzqct=/shtml/ssq/03089%2Eshtml; _qzjb=1.1537155732535.1.0.0.0; __utma=63332592.1675891417.1537149673.1537152883.1537155733.3; sdc_userflag=1537155732277::1537157402642::7; Hm_lpvt_4f816d475bb0b9ed640ae412d6b42cab=1537157403; _jzqb=1.7.10.1537155733.1; __utmt=1; __utmb=63332592.7.10.1537155733; CLICKSTRN_ID=1.80.218.173-1537149664.528767::16B1D937ED722FF6F94810E7758DDA62; motion_id=1537157412260_0.12137685854805791");
             httpGet.addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
             httpGet.addHeader("Accept-Encoding", "gzip, deflate");
             try {
@@ -68,6 +76,7 @@ public class SimpleCaiPiao {
             }
         }
     }
+
     public static String convertStreamToString(InputStream is) {
         StringBuilder sb = new StringBuilder();
         String line = null;
